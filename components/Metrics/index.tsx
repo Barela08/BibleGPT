b192d3dc4e035f2cfa881d9de279dd16822cc7b4
barela08 <nbgamer126@gmail.com>
2025-04-01 00:45:06 -0700

    first commit

---
+++ components/Metrics/index.tsx
@@ -0,0 +1,11 @@
+import HotJarAnalytic from "./HotJarAnalytic";
+import ClarityAnalytic from "./ClarityAnalytic";
+
+const Metrics = () => (
+  <>
+    <HotJarAnalytic />
+    <ClarityAnalytic />
+  </>
+);
+
+export default Metrics;
---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/Barela08",
+        permanent: false,
+      },
+    ];
+  },
+};
